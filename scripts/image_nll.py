f3b2bd0676c2bf9fcff6ecf979f66234880d12b5
chesha1 <chesha1@163.com>
2023-07-13 16:06:12 +0800

    add comment

--- scripts/image_nll.py
+++ scripts/image_nll.py
@@ -1,5 +1,6 @@
 """
 Approximate the bits/dimension for an image model.
+这段代码用于对图像模型进行比特/维度的近似评估
 """
 
 import argparse
@@ -48,30 +49,43 @@ def main():
 
 
 def run_bpd_evaluation(model, diffusion, data, num_samples, clip_denoised):
+    # 创建一个空列表all_bpd用于存储每个样本的比特/维度指标
     all_bpd = []
+
+    # 创建一个字典all_metrics，其中包含了三个术语指标的空列表，分别是"vb"、"mse"和"xstart_mse
     all_metrics = {"vb": [], "mse": [], "xstart_mse": []}
+
+    # 初始化变量num_complete为0，用于追踪已完成的样本数量
     num_complete = 0
     while num_complete < num_samples:
+
+        # 从数据加载器中获取下一个批次的数据和模型参数
         batch, model_kwargs = next(data)
         batch = batch.to(dist_util.dev())
         model_kwargs = {k: v.to(dist_util.dev()) for k, v in model_kwargs.items()}
+
+        # 使用扩散对象的calc_bpd_loop方法计算每个批次的比特/维度指标
         minibatch_metrics = diffusion.calc_bpd_loop(
             model, batch, clip_denoised=clip_denoised, model_kwargs=model_kwargs
         )
-
+        # 对每个指标的术语列表进行归约操作，计算每个术语的平均值，并将其添加到all_metrics字典中
         for key, term_list in all_metrics.items():
             terms = minibatch_metrics[key].mean(dim=0) / dist.get_world_size()
             dist.all_reduce(terms)
             term_list.append(terms.detach().cpu().numpy())
 
+        # 计算总的比特 / 维度指标并归约
         total_bpd = minibatch_metrics["total_bpd"]
         total_bpd = total_bpd.mean() / dist.get_world_size()
         dist.all_reduce(total_bpd)
+
+        # 将总的比特 / 维度指标添加到all_bpd列表中
         all_bpd.append(total_bpd.item())
         num_complete += dist.get_world_size() * batch.shape[0]
 
         logger.log(f"done {num_complete} samples: bpd={np.mean(all_bpd)}")
 
+    # 如果当前进程的排名为0，则对每个指标的术语列表进行平均，并将结果保存到磁盘上的文件中
     if dist.get_rank() == 0:
         for name, terms in all_metrics.items():
             out_path = os.path.join(logger.get_dir(), f"{name}_terms.npz")
--- scripts/image_sample.py
+++ scripts/image_sample.py
@@ -41,8 +41,17 @@ def main():
     logger.log("sampling...")
     all_images = []
     all_labels = []
+
+    # 在生成足够数量的样本之前，循环执行以下步骤：
+
+
+
+
+
     while len(all_images) * args.batch_size < args.num_samples:
         model_kwargs = {}
+
+        # 如果启用了条件生成（class_cond），随机生成一个类别标签，并将其传递给模型。
         if args.class_cond:
             classes = th.randint(
                 low=0, high=NUM_CLASSES, size=(args.batch_size,), device=dist_util.dev()
@@ -51,16 +60,23 @@ def main():
         sample_fn = (
             diffusion.p_sample_loop if not args.use_ddim else diffusion.ddim_sample_loop
         )
+
+        # 根据给定的模型和输入形状生成一个样本。
         sample = sample_fn(
             model,
             (args.batch_size, 3, args.image_size, args.image_size),
             clip_denoised=args.clip_denoised,
             model_kwargs=model_kwargs,
         )
+
+        # 对生成的样本进行后处理（例如，重新缩放、限制像素值范围）。
         sample = ((sample + 1) * 127.5).clamp(0, 255).to(th.uint8)
         sample = sample.permute(0, 2, 3, 1)
         sample = sample.contiguous()
 
+        # 使用分布式训练工具将样本收集到每个进程的列表中。
+        # 如果启用了条件生成，还要收集类别标签。
+        # 记录生成的样本数量。
         gathered_samples = [th.zeros_like(sample) for _ in range(dist.get_world_size())]
         dist.all_gather(gathered_samples, sample)  # gather not supported with NCCL
         all_images.extend([sample.cpu().numpy() for sample in gathered_samples])
@@ -72,11 +88,16 @@ def main():
             all_labels.extend([labels.cpu().numpy() for labels in gathered_labels])
         logger.log(f"created {len(all_images) * args.batch_size} samples")
 
+    # 将收集到的样本列表合并为一个大的NumPy数组
     arr = np.concatenate(all_images, axis=0)
     arr = arr[: args.num_samples]
+
+    # 如果启用了条件生成，将类别标签也合并为一个NumPy数组
     if args.class_cond:
         label_arr = np.concatenate(all_labels, axis=0)
         label_arr = label_arr[: args.num_samples]
+
+    # 如果当前进程的排名为0，则保存生成的样本数组（以及类别标签数组，如果有）到磁盘上的一个文件
     if dist.get_rank() == 0:
         shape_str = "x".join([str(x) for x in arr.shape])
         out_path = os.path.join(logger.get_dir(), f"samples_{shape_str}.npz")
@@ -86,6 +107,8 @@ def main():
         else:
             np.savez(out_path, arr)
 
+    # 同步所有进程。
+    # 记录生成过程完成
     dist.barrier()
     logger.log("sampling complete")
 
